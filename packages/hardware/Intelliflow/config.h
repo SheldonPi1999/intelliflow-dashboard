ed4de06de22431df4cea9bda72a361895e80da56
SheldonPi1999 <55874520+SheldonPi1999@users.noreply.github.com>
2021-05-10 11:30:05 +0200

    Combined 4 Included

--- packages/hardware/Intelliflow/config.h
+++
@@ -1,4 +0,0 @@
-const char* ssid = "SSID"; //change this to the SSID of your Wi-Fi network
-const char* password = "*********"; //change this to the password of your Wi-Fi network
-const char* mqtt_server = "home.iwg-it.com"; // change this to your mqtt broker url or ip
-const int mqtt_port = 24075; //change this to your mqtt broker port
