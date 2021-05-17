5462ed1f26b13d22046907f57802b580e1ca7ed9
SheldonPi1999 <55874520+SheldonPi1999@users.noreply.github.com>
2021-05-17 21:54:28 +0200

    Update

--- rework/hardware/build/cmake_install.cmake
+++ rework/hardware/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Stage/GitHub/IntelliFlow/rework/hardware
+# Install script for directory: /Users/macbookkasper/Documents/Stage/IntelliFlow/rework/hardware
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Intelliflow-Sensor-Nodes")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,9 +32,14 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/Users/macbookkasper/.espressif/tools/xtensa-esp32-elf/esp-2020r3-8.4.0/xtensa-esp32-elf/bin/xtensa-esp32-elf-objdump")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("D:/Stage/GitHub/IntelliFlow/rework/hardware/build/esp-idf/cmake_install.cmake")
+  include("/Users/macbookkasper/Documents/Stage/IntelliFlow/rework/hardware/build/esp-idf/cmake_install.cmake")
 
 endif()
 
@@ -46,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Stage/GitHub/IntelliFlow/rework/hardware/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/macbookkasper/Documents/Stage/IntelliFlow/rework/hardware/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- rework/hardware/build/esp-idf/mbedtls/mbedtls/cmake_install.cmake
+++ rework/hardware/build/esp-idf/mbedtls/mbedtls/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/ESP-IDF/components/mbedtls/mbedtls
+# Install script for directory: /Users/macbookkasper/esp/esp-idf/components/mbedtls/mbedtls
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Intelliflow-Sensor-Nodes")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,10 +32,15 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/Users/macbookkasper/.espressif/tools/xtensa-esp32-elf/esp-2020r3-8.4.0/xtensa-esp32-elf/bin/xtensa-esp32-elf-objdump")
+endif()
+
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("D:/Stage/GitHub/IntelliFlow/rework/hardware/build/esp-idf/mbedtls/mbedtls/library/cmake_install.cmake")
-  include("D:/Stage/GitHub/IntelliFlow/rework/hardware/build/esp-idf/mbedtls/mbedtls/include/cmake_install.cmake")
+  include("/Users/macbookkasper/Documents/Stage/IntelliFlow/rework/hardware/build/esp-idf/mbedtls/mbedtls/library/cmake_install.cmake")
+  include("/Users/macbookkasper/Documents/Stage/IntelliFlow/rework/hardware/build/esp-idf/mbedtls/mbedtls/include/cmake_install.cmake")
 
 endif()
 
